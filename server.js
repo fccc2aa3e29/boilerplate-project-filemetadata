947c5c4940c141d6f8715c9b8f51412973421ce4
fccc2aa3e29 <49536873+fccc2aa3e29@users.noreply.github.com>
2019-04-25 04:00:00 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
-
+const multer = require('multer');
 var app = express();
 
 app.use(cors());
@@ -16,6 +16,15 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+const upload = multer({dest: 'uploads/'});
+const del = require('del');
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res, next) => {
+  if(!req.file) return res.send("error");
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+  del.sync(['uploads/**', '!uploads/']);
 });
 
 app.listen(process.env.PORT || 3000, function () {
